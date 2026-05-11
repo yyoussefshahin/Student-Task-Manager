faeaf4f1a80df962cd7e97444dbd7170e48e3bba
Youssefyasser-cs <youssefyaser442@gmail.com>
2026-05-12 01:15:32 +0200

    Add files via upload

---
+++ UserRepository.java
@@ -0,0 +1,10 @@
+import com.project.TaskManger.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    List<User> findByRole(User.Role role);
+}
